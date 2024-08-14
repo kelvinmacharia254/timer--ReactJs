cfb7380bbc031855cfebbbecc1cd0131fa13bd9d
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-08-14 16:23:02 +0300

    feat: start button text conditional rendering
    - reuse same button form start, stop and resume by monitoring state of the time.
    - delete time components as experiment(introduce later as app grows)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,64 @@
 
 import timerLogo from '/time-svgrepo-com.svg'
-import Timer from "./assets/components/Timer.jsx";
+import {useRef, useState} from "react";
 
 
 function App() {
-    console.log("Render App")
+    const [time, setTime] = useState(0)
+    const timerRef = useRef(null)
+    const [buttonText, updateButtonText] = useState('Start')
+
+    function handleTimer(buttonIdentity){
+        if(!timerRef.current){ // if no timer is running
+            if (buttonIdentity === 'Start') {
+                console.log(`Time started: ${timerRef.current}`)
+                timerRef.current = setInterval(()=>{
+                setTime((prevTime)=>prevTime+1000)
+                },1000)
+                updateButtonText('Stop')
+            }else{
+                console.log(`Timer resumed: ${timerRef.current}`)
+                timerRef.current = setInterval(()=>{
+                setTime((prevTime)=>prevTime+1000)
+                },1000)
+                updateButtonText('Stop')
+            }
+        }else{ // if there is a time
+            console.log(`Timer stopped before clearing interval: ${timerRef.current}`)
+            clearInterval(timerRef.current)
+            timerRef.current = null
+            updateButtonText('Resume')
+            console.log(`Timer stopped after clearing interval: ${timerRef.current}`)
+        }
+    }
+
+    function handleResetTimer() {
+        clearInterval(timerRef.current)
+        timerRef.current = null
+        setTime(0)
+        updateButtonText('Start')
+    }
+
   return (
       <>
           <div id="overlay"></div>
           <div id="content">
+
               <div id="header-image">
-                  <img src={timerLogo} className="logo" alt="Vite logo"/>
+                  <img src={timerLogo} className="logo" alt="timer logo"/>
               </div>
+
               <h1>The ultimate Timer</h1>
-              <Timer/>
+
+              <div id="time-container">
+                  <h1 id="time-head">{time / 1000}</h1>
+              </div>
+              <div>
+                  <button onClick={() => {
+                      handleTimer(buttonText)
+                  }} value={buttonText.current}>{buttonText}</button>
+                  <button onClick={handleResetTimer}>Reset</button>
+              </div>
           </div>
       </>
   )
